df32cc5f65e0b42cdd16646eb17dafe8bb0fba64
Dagny Grabe <dagnygrabe@Dagnys-MacBook-Pro.local>
2022-10-21 12:28:18 +0200

    create app and install tailwind

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl text-red-500">hello</h1>
+      <p>hello2</p>
+    </div>
+  );
+}
+
+export default App;
