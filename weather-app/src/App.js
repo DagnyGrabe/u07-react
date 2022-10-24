9afca77926ac85674b72811d694537cd8fb9f3ad
Dagny Grabe <dagnygrabe@Dagnys-MacBook-Pro.local>
2022-10-24 13:39:26 +0200

    get weather for user position

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,59 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import CurrentWeather from "./components/current-weather";
 
 
 function App() {
 
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
-  const [error, setError] = useState(null);
 
-  const geolocationAPI = navigator.geolocation;
+  const getUserCoordinates = async () => {
 
-  useEffect(() => {
-    const getUserCoordinates = () => {
-      if (!geolocationAPI) {
-        setError('Geolocation API is not available in your browser!')
-      } else {
-        geolocationAPI.getCurrentPosition((position) => {
-          const { coords } = position;
-          setLat(coords.latitude);
-          setLon(coords.longitude);
-        }, (error) => {
-          setError('Something went wrong getting your position!')
-        })
-      }
-      console.log(error);
+    const getCoords = () => new Promise((success, error) => 
+    navigator.geolocation.getCurrentPosition(success, error));
+
+    try {
+      const response = await getCoords();
+      setLat(response.coords.latitude);
+      setLon(response.coords.longitude);
+    } catch (err) {
+      console.log(err);
     }
-    getUserCoordinates();
-  }, []);
+
+  }
+
 
   const currentWeatherUrl =
-    `${WEATHER_API_URL}/weather?lat=59.32938&lon=18.06871&appid=${WEATHER_API_KEY}&units=metric`;
+    `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
 
   const [currentWeather, setCurrentWeather] = useState(null);
 
-  useEffect(() => {
-    const getCurrentWeather = async () => {
-      try {
-        const response = await axios.get(currentWeatherUrl);
-        console.log(response);
-        setCurrentWeather(response.data);
-      } catch (err) {
-        console.log(err);
-      }
+
+  const getCurrentWeather = async () => {
+    try {
+      const response = await axios.get(currentWeatherUrl);
+      console.log(response);
+      setCurrentWeather(response.data);
+    } catch (err) {
+      console.log(err);
     }
-
-    getCurrentWeather();
-  }, []);
-
-
+  }
 
   return (
     <div className="App">
       <h1 className="text-2xl text-red-500">hello</h1>
-      <p>{lat}, {lon}</p>
+      <button
+        onClick={() => getUserCoordinates()}>Use my location</button>
+      {currentWeather && <CurrentWeather data={currentWeather} />}
 
 
     </div>
--- weather-app/src/components/current-weather.js
+++ weather-app/src/components/current-weather.js
@@ -4,10 +4,21 @@ const CurrentWeather = ({data}) => {
     return (
         <div>
             <div>
-                <p>{data.city}</p>
+                <p>{data.name}</p>
+                <p>{data.weather[0].description}</p>
+                <img 
+                alt="weather-icon"
+                src={require(`../icons/${data.weather[0].icon}.png`)} />
+                <p>{Math.round(data.main.temp)}°C</p>
+            </div>
+            <div>
+                <p>details</p>
+                <ul>
+                    
+                </ul>
             </div>
         </div>
     )
 }
 
-export default CurrentWeather();
+export default CurrentWeather;
